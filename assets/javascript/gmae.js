c627e2c0e65e81c48c49c2487abacdfe4cb9ea6a
Yaraye <yaraye@gmail.com>
2018-06-01 22:10:58 -0400

    changes

--- assets/javascript/gmae.js
+++ assets/javascript/gmae.js
@@ -1,11 +1,11 @@
-alert("you got it");
+
 //declearing variable and assigning the variable 
-var secretWords=["LightMyFire","PeopleAreStrange", "LoveStreet", "IndianSummer"];
+var secretWords=["lightmyfire","peoplearestrange", "lovestreet", "indiansummer"];
 
 //global var
 var wins = 0;
 var losses = 0;
-var currentWord = [];
+var currentWords = [];
 var numberOfGuesesLeft= 9;
 var letterAlreadyGuessed = [];
 var pickOneWord = "";
@@ -15,14 +15,76 @@ var rightLetterCount = 0;
 
 function StartGame()
 {
-//     pickOneWord = secretWords[Math.floor(Math.random() * secretWords.length)];
-//     rightLetterCount = pickOneWord.length;
 
-//     console.log(pickOneWord);
-
+	pickOneWord = secretWords[Math.floor(Math.random() * secretWords.length)];
+	rightLetterCount = pickOneWord.length;
+	console.log(pickOneWord);
+	
 	//reset values
 	numberOfGuesesLeft = 9;
-	letterAlreadyGuessed= [];
-	currentWord = [];
+	currentWords = [];
+	letterAlreadyGuessed = [];
+
+	document.querySelector("#wins").innerHTML = wins;
+	document.querySelector("#losses").innerHTML = losses;
+
+	for(var i =0; i < pickOneWord.length; i++)
+	{
+		currentWords.push("_");
+	}
+
+	document.querySelector("#cwords").innerHTML = currentWords.join(" ");
+
+
 }
+
+function CompareLetter(word)
+{
+
+  letterAlreadyGuessed.push(word);
+  document.querySelector("#guessed").innerHTML = letterAlreadyGuessed.join(" ");
+  var correctGuess = false;
+
+  for(var i=0;i<pickOneWord.length; i++)
+  {
+  	if(word == pickOneWord[i])
+  	{
+  		correctGuess = true;
+  		currentWords[i] = word;
+  		document.querySelector("#cwords").innerHTML = currentWords.join(" ");
+  		rightLetterCount--;
+  		
+  	}
+
+  }
+
+
+  if(correctGuess == false)
+  {
+  	numberOfGuesesLeft--;
+  }
+
+  if(numberOfGuesesLeft == 0)
+  {
+  	losses++;
+  	document.querySelector("#losses").innerHTML = losses;
+  }
+
+  if(rightLetterCount == 0)
+  {
+  	wins++;
+  	document.querySelector("#wins").innerHTML = wins;
+  }
+
+}
+
 StartGame();
+document.onkeyup = function(event){
+
+	if(event.keyCode >= 65 && event.keyCode <= 90)
+	{
+		var letter = String.fromCharCode(event.keyCode).toLowerCase();
+		//console.log(letter);
+		CompareLetter(letter);
+	}
+	}
